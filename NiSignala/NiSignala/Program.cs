7357e7418fa6888da558611cce8e8c0e8ac183cd
Estej Samsa Šraml <estej.samsa@gmail.com>
2022-10-13 10:14:36 +0200

    Massage

---
+++ NiSignala/NiSignala/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiSignala
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kupec janez = new Kupec();
+            janez.Ime = "Janez Novak";
+            janez.BeležiKlic(10, TipKlica.Stacionarno);
+            Console.WriteLine(janez.Ime + " dolguje " + janez.Stanje);
+            Kupec alenka = new Kupec();
+            alenka.Ime = "Alenka Novak";
+            alenka.BeležiKlic(10, TipKlica.Mobilno);
+            Console.WriteLine(alenka.Ime + " dolguje " + alenka.Stanje);
+            Console.ReadLine();
+        }
+    }
+}
